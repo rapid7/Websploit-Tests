53faf3cb608757af56f1be17355218a508706876
Tasos Laskos <Tasos_Laskos@rapid7.com>
2012-11-26 19:45:45 +0200

    php_include: updated to wrap the output of the included file in HTML code

--- php_include/vuln.php
+++ php_include/vuln.php
@@ -4,7 +4,24 @@ ini_set( 'allow_url_include', 1 );
 
 $input = stripslashes( $input );
 
-echo $input;
+$rand  = rand( 0, 999 );
+$rand2 = rand( 0, 999 );
 
-if( $input ){ echo include( $input ); }
+echo <<<EOT
+<html>
+    <head>
+        <title>My title...</title>
+    </head>
+    <body>
+        <h1>Stuff...</h1>
+        <h2>This is random: $rand</h2>
+        <pre>
+EOT;
 
+if( $input ){ include( $input ); }
+    
+echo "This is random too -- $rand2
+</pre>
+    </body>
+</html>
+";
